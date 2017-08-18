f12f2824949bbc5548319a6f5ac1ecdba6204bd5
soybean <warau@binary.to>
2017-08-18 15:55:56 +0200

    add a method do_request

--- SU/API/Grafana.pm
+++ SU/API/Grafana.pm
@@ -13,7 +13,7 @@ use URI::Escape;
 
 sub new {
     my($class, $args) = @_;
-    my $auth = "Authorization: Bearer " . $args->{api_key};
+    my $auth = "Bearer " . $args->{api_key};
     my $self = {
         hostname => $args->{hostname},
         auth     => $auth,
@@ -23,6 +23,20 @@ sub new {
 
     bless $self, $class;
     return $self;
+}
+
+sub do_request {
+    my ($self,$dashboard) = @_;
+
+    my $content_type = "application/json";
+    my $uri = URI->new("$self->{url}/api/dashboards/db/$dashboard");
+    my $req = HTTP::Request->new('GET' => "$uri");
+    $req->content_type($content_type);
+    $req->header( Authorization => $self->{auth} );
+    $req->header( Accept => $content_type );
+
+    my $ua = LWP::UserAgent->new;
+    my $response = $ua->request($req);
 }
 
 1;
